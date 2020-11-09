e3eb99e787658d01d50b72c26e9f1a17879b3385
Michał Duda <michal.duda@comp.com.pl>
2020-11-09 22:37:03 +0100

    Create test class for electicityMeter

---
+++ TDD_Course/src/test/java/pl/mduda/tdd/electicity/ElectricityMeterTest.java
@@ -0,0 +1,12 @@
+package pl.mduda.tdd.electicity;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ElectricityMeterTest {
+
+    @Test
+    public void addKwh() {
+    }
+}
